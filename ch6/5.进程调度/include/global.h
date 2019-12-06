0bd01a334a994c69428142da4c6c469042f0683d
ye <yeholdon@gmail.com>
2019-12-06 15:15:12 +0800

    process finished

---
+++ ch6/5.进程调度/include/global.h
@@ -0,0 +1,38 @@
+/**************************************************************
+ * global.h
+ * 程序功能：存放全局变量的声明
+ * 修改日期：2019.11.29
+ */
+#ifndef _GLOBAL_H_
+#define _GLOBAL_H_
+
+#include "proc.h"
+/* EXTERN is defined as extern except in global.c */
+#ifdef	GLOBAL_VARIABLES_HERE
+#undef	EXTERN
+#define	EXTERN
+#endif
+
+EXTERN  int ticks;
+
+EXTERN	int		disp_pos;
+EXTERN  u8  gdt_ptr[6];	/* 0~15:Limit  16~47:Base */
+EXTERN DESCRIPTOR  gdt[GDT_SIZE];
+EXTERN	u8		idt_ptr[6];	/* 0~15:Limit  16~47:Base */
+EXTERN	GATE		idt[IDT_SIZE];
+
+EXTERN  u32 k_reenter;
+
+EXTERN  PROCESS *p_proc_ready;
+EXTERN  TSS tss;
+// 定义global.c里, 这里声明过后才能在别的地方引用
+extern  PROCESS proc_table[];
+extern  char    task_stack[];
+
+extern  TASK    task_table[];
+
+extern  irq_handler irq_table[];
+
+extern  system_call sys_call_table[NR_SYS_CALL];
+
+#endif
---
+++ ch6/5.进程调度/include/string.h
@@ -0,0 +1,9 @@
+/**************************************************************
+ * string.h
+ * 程序功能：模仿C语言库的string.h放字符串操作相关的函数
+ * 修改日期：2019.11.28
+ */
+
+PUBLIC	void*	memcpy(void* p_dst, void* p_src, int size);
+PUBLIC	void	memset(void* p_dst, char ch, int size);
+PUBLIC  char* strcpy(char* p_dst, char* p_src);
