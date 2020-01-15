5944593b4252325633a44238446ee5358d119c05
ye <yeholdon@gmail.com>
2020-01-15 14:30:54 +0800

    move printf.c/vsprintf.c from kernel/ to lib/

---
+++ ch10/2. memery_manage_exce/mklib.sh
@@ -0,0 +1,3 @@
+ar rcs lib/yescrt.a lib/syscall.o lib/printf.o lib/vsprintf.o lib/string.o \
+lib/misc.o lib/open.o lib/read.o lib/write.o lib/close.o lib/unlink.o \
+lib/getpid.o lib/fork.o lib/exit.o lib/wait.o 
